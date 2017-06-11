6a307e00406b0dc73df5a643706bfccd425318ce
Douwe van Steenis <dst1995@hotmail.nl>
2017-06-11 13:31:24 +0200

    classification 2

--- Classification.R
+++ Classification.R
@@ -41,6 +41,8 @@ corpus.clean <- corpus %>%
 dtm <- DocumentTermMatrix(corpus.clean) #standard functie uit tm library
 inspect(dtm[40:50, 10:15])
 
+#===
+
 #partitioning the data
 df.train <- df[1:1500,]
 df.test <- df[1501:2000,]
@@ -51,3 +53,44 @@ dtm.test <- dtm[1501:2000,]
 corpus.clean.train <- corpus.clean[1:1500]
 corpus.clean.test <- corpus.clean[1501:2000]
 
+#Feature selection, woorden eruit filteren die minder dan 5 reviews voorkomen
+dim(dtm.train)
+
+fivefreq <- findFreqTerms(dtm.train, 5)
+length((fivefreq))
+
+dtm.train.nb <- DocumentTermMatrix(corpus.clean.train, control=list(dictionary = fivefreq))
+
+dim(dtm.train.nb)
+
+dtm.test.nb <- DocumentTermMatrix(corpus.clean.test, control=list(dictionary = fivefreq))
+
+dim(dtm.train.nb)
+
+# Function to convert the word frequencies to yes (presence) and no (absence) labels
+convert_count <- function(x) {
+  y <- ifelse(x > 0, 1,0)
+  y <- factor(y, levels=c(0,1), labels=c("No", "Yes"))
+  y
+}
+
+# Apply the convert_count function to get final training and testing DTMs
+trainNB <- apply(dtm.train.nb, 2, convert_count)
+testNB <- apply(dtm.test.nb, 2, convert_count)
+
+# Train the classifier
+system.time( classifier <- naiveBayes(trainNB, df.train$class, laplace = 1) )
+
+# Use the NB classifier we built to make predictions on the test set.
+system.time( pred <- predict(classifier, newdata=testNB) )
+
+# Create a truth table by tabulating the predicted class labels with the actual class labels 
+table("Predictions"= pred,  "Actual" = df.test$class )
+
+# Prepare the confusion matrix
+conf.mat <- confusionMatrix(pred, df.test$class)
+conf.mat
+
+conf.mat$byClass
+conf.mat$overall
+conf.mat$overall['Accuracy']
